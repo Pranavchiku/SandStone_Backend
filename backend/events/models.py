94b845294b9a4e18561edb00c2c9cbb409b26c19
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-15 00:17:06 +0530

    Added model for speakers

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Speaker(models.Model):
+    speaker_name=models.CharField(max_length=100)
+    speaker_company=models.CharField(max_length=100)
+    speaker_position=models.CharField(max_length=100)
+    speaker_date=models.DateField()
+    speaker_image=models.ImageField(upload_to='images/', blank=True)

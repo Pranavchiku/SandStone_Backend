aaefe9aff2104676441280b09431b7b9d81b79ca
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-15 00:18:29 +0530

    Added api endpoint getspeaker

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.core import serializers
+from .models import *
+from django.http import JsonResponse
 # Create your views here.
+
+def getSpeaker(request):
+    #serialze the data from the model speakers
+    data = serializers.serialize('json', Speaker.objects.all())
+    data={"speakers":data}
+    res=JsonResponse(data)
+    return res
+
